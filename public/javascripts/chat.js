0cf3d7d4160b193e88604912cc2c4b4e357585d9
amberwoodrow <amberwoodrow@gmail.com>
2015-11-12 15:43:36 -0700

    scroll working

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,12 +1,16 @@
 $(function() {
   var socket = io();
   var username;
+  $('.pages').addClass('border-radius');
 
   $(".name-form").submit(function(e) {
     e.preventDefault();
     // console.log($(".usernameInput").val());
     username = $(".usernameInput").val().trim();
     $('.login').hide();
+    $('.pages').removeClass('border-radius');
+    $('.message-area').addClass('border-radius');
+    $('.message-area').show();
     $('.chat-container').show();
     $("#someone-entered").fadeOut("slow");
   });
@@ -21,6 +25,11 @@ $(function() {
   socket.on('data', function (frogs) {
     console.log(frogs);
     $("#messages").append('<li><span class="color">'+frogs.username+": </span>"+frogs.chatMessage+'</li>');
+    $(function() {
+      var scroll = $('#scroll');
+      var height = scroll[0].scrollHeight;
+      scroll.scrollTop(height);
+    });
   });
 
   // var element = $("#chat")[0];
